9d6141de231fc2a3db10f10d024c053e10235a29
Alex Hirsch <alex@dps.uibk.ac.at>
2020-03-31 21:40:29 +0200

    Add matplotlib example

---
+++ images/plot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)')
+ax.grid()
+
+fig.set_size_inches(4, 3.5)
+fig.savefig('plot.pgf')
